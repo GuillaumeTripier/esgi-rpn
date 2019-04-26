706e48dcbbffbdc0a631aee56819dcdf007b2013
G-T <guillaume.tripier1@gmail.com>
2019-04-26 14:42:08 +0200

    init repo

--- src/main/java/rpn/CLI.java
+++ src/main/java/rpn/CLI.java
@@ -12,7 +12,43 @@ public class CLI {
         System.out.println("> " + result);
     }
 
-    static long evaluate(String expression) {
+    /*static long evaluate(String expression) {
         return 0;
+    }*/
+    static long evaluate(String entree){//ArrayDeque
+        String[] fullString = entree.split(" ");
+        int result = 0;
+        int op1;
+        int op2;
+        char c;
+        result = Integer.parseInt(fullString[0]);
+        if(fullString.length > 2) {
+            for (int i = 0; i < fullString.length - 2; i += 2) {
+                op1 = result;
+                op2 = Integer.parseInt(fullString[i + 1]);
+                System.out.println(op2);
+                //c = fullString[i + 2].charAt(0);
+                c = fullString[fullString.length/2].charAt(0);
+                switch (c) {
+                    case '+':
+                        result = op1 + op2;
+                        break;
+                    case '-':
+                        result = op1 - op2;
+                        break;
+                    case '*':
+                        result = op1 * op2;
+                        break;
+                    case '/':
+                        result = op1 / op2;
+                        break;
+                    default:
+                        i += fullString.length;
+                        System.out.println("Error");
+                }
+            }
+        }
+        //System.out.println(result);
+        return result;
     }
 }
